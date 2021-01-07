7d0a2838409dc286f0ec7930dfab62b526a82349
tiket135 <tiket135@gmail.com>
2021-01-07 19:09:30 +0100

    Task 2 - Home page

--- web/src/views/Home.vue
+++ web/src/views/Home.vue
@@ -1,10 +1,31 @@
 <template>
-  <div class="home"></div>
+  <div class="home">
+    <h2>This is a home page</h2>
+    <img class="mt-8 mb-8" alt="logo" src="../assets/logo_blue.svg" />
+    <p class="text">
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Beatae debitis
+      deleniti dignissimos dolorem eligendi est expedita in ipsam magnam, nam
+      obcaecati, omnis optio porro quaerat quia, suscipit vel vero voluptatibus.
+    </p>
+  </div>
 </template>
 
 <script>
 export default {
-  name: "Home",
-  components: {}
+  name: "Home"
 };
 </script>
+
+<style lang="scss">
+.home {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  padding: 60px 15px;
+}
+.text{
+  max-width: 600px;
+  text-align: center;
+}
+</style>
